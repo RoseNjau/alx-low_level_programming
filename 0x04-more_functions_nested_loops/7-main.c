3df1baf266e3e87dc6bfcabec368c55c10c5553b
Rose Njau <rosenjau0@gmail.com>
2023-06-22 19:30:44 -0700

    7-print_diagonal.c A fuction that draws a diagonal line on the terminal.

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -13,19 +13,3 @@ int main(void)
     print_diagonal(-4);
     return (0);
 }
-julien@ubuntu:~/0x04$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 7-main.c 7-print_diagonal.c -o 7-diagonals
-julien@ubuntu:~/0x04$ ./7-diagonals | cat -e
-$
-\$
- \$
-\$
- \$
-  \$
-   \$
-    \$
-     \$
-      \$
-       \$
-        \$
-         \$
-$
