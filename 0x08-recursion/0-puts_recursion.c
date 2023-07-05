81133e48ad842fbd1be0b8236905fbb16a423b5e
Rose Njau <rosenjau0@gmail.com>
2023-07-06 01:55:03 +0300

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _puts_recursion - function that prints a string, followed by a new line();
  * @s: input
- * Return: Always 0 (void) 
+ * Return: Always 0 (void)
  */
 void _puts_recursion(char *s)
 {
