76fe70e0abf32513937de1879318164e69008657
Rose Njau <rosenjau0@gmail.com>
2023-07-10 15:30:01 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -6,45 +6,37 @@
  * argstostr - Concatenates all the arguments of the program
  * @ac: The number of arguments
  * @av: Array of argument strings
+ *
  * Return: Pointer to the concatenated string, or NULL if it fails
  */
 char *argstostr(int ac, char **av)
 {
-char *str;
-int i, j, len, total_len = 0, index = 0;
+	char *str;
+	int i, j, len, total_len = 0, index = 0;
 
-if (ac == 0 || av == NULL)
-return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-for (i = 0; i < ac; i++)
-{
-len = strlen(av[i]);
-total_len += len;
+	for (i = 0; i < ac; i++)
+		total_len += strlen(av[i]);
+
+	total_len += ac - 1;
+
+	str = malloc((total_len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		len = strlen(av[i]);
+		for (j = 0; j < len; j++)
+			str[index++] = av[i][j];
+		if (i < ac - 1)
+			str[index++] = '\n';
+	}
+
+	str[index] = '\0';
+
+	return (str);
 }
-
-total_len += ac - 1;
-
-str = malloc((total_len + 1) * sizeof(char));
-if (str == NULL)
-return (NULL);
-
-for (i = 0; i < ac; i++)
-{
-len = strlen(av[i]);
-for (j = 0; j < len; j++)
-{
-str[index++] = av[i][j];
-}
-if (i < ac - 1)
-str[index++] = '\n';
-}
-
-str[index] = '\0';
-
-return (str);
-}
-
-
-
-
 
